74effd9f547b3c5e88f6703bf683d3c91fd194d5
Ben Limmer <ben@benlimmer.com>
2023-08-25 11:17:57 -0600

    chore: show routing issue with expo go links

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import { createURL } from 'expo-linking';
+import { createURL, getInitialURL } from 'expo-linking';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const link = createURL('/')
 
+  const [initialUrl, setInitialUrl] = useState<string | null>(null);
+  getInitialURL().then(setInitialUrl);
+
   return (
     <View style={styles.container}>
       <Text>The link is {link}</Text>
+      <Text>The initial URL was {initialUrl}</Text>
       <StatusBar style="auto" />
     </View>
   );
